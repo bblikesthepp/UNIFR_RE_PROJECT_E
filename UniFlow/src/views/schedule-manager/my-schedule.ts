77fb1e9afdd4c95243c20c3a3f29a9442baf457f
“bblikesthepp” <ana.bog@unifr.ch>
2024-12-17 22:43:59 +0100

    scheduler added

--- UniFlow/src/views/schedule-manager/my-schedule.ts
+++ UniFlow/src/views/schedule-manager/my-schedule.ts
@@ -1,16 +1,32 @@
 import { customElement } from '@aurelia/runtime-html';
 import template from './my-schedule.html';
 import '@aurelia-mdc-web/all';
+import { inject } from 'aurelia';
+import { SharedService, Course } from '../../resources/shared-service';
 
+@inject(SharedService)
 @customElement({
   name: 'my-schedule',
-  template
+  template,
 })
 export class MySchedule {
   public title: string = 'My Schedule';
-  public schedule = [
-    { time: '08:00 AM', course: 'Math 101' },
-    { time: '10:00 AM', course: 'History 201' },
-    { time: '01:00 PM', course: 'Physics 303' },
-  ];
+  public schedule: { day: string; time: string; course: string }[] = [];
+
+  private weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+  constructor(private sharedService: SharedService) {}
+
+  attached() {
+    const timeSlots = ['08:00 AM', '10:00 AM', '01:00 PM', '03:00 PM'];
+
+    const courses = this.sharedService.getCourses();
+
+    // automatically put courses into the schedule
+    this.schedule = courses.map((course, index) => ({
+      day: this.weekDays[index % this.weekDays.length], // rotate thru weekdays
+      time: timeSlots[index % timeSlots.length],       // rotate thru time slots
+      course: course.name,
+    }));
+  }
 }
